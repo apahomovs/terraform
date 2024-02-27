fd16d2fdcb8e2a28c0b4b97099992c0f9df9436b
apahomovs <alexanderpahomovs@gmail.com>
2024-02-26 18:01:47 -0600

    updated

--- lesson6_modules/main.tf
+++ lesson6_modules/main.tf
@@ -8,17 +8,17 @@ module "subnet_1a" {
   source = "./subnet_module"
 
   for_each = {
-    "public_1a" = ["10.0.0.0/26", "us-east-1a", true]
-    "public_1b" = ["10.0.0.64/26", "us-east-1a", true]
+    "public_1a"  = ["10.0.0.0/26", "us-east-1a", true]
+    "public_1b"  = ["10.0.0.64/26", "us-east-1a", true]
     "private_1a" = ["10.0.0.128/26", "us-east-1a", false]
-    "private_1b" = ["10.0.0.196/26", "us-east-1a", false]
+    "private_1b" = ["10.0.0.192/26", "us-east-1a", false]
   }
 
-  custom_vpc_id = module.dev_vpc.vpc_id
-  custom_subnet_tag = each.key
-  custom_availability_zone = each.value[1]
+  custom_vpc_id                  = module.dev_vpc.vpc_id
+  custom_subnet_tag              = each.key
+  custom_availability_zone       = each.value[1]
   custom_map_public_ip_on_launch = each.value[2]
-  custom_cidr_block = each.value[0]
+  custom_cidr_block              = each.value[0]
 }
 
 
